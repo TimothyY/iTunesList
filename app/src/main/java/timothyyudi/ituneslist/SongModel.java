ec35374dacdf7be20a82caede35d0605f310d362
Timothy Yudi <timothyyudi@gmail.com>
2016-12-20 18:13:49 +0700

    Initial commit

---
+++ app/src/main/java/timothyyudi/ituneslist/SongModel.java
@@ -0,0 +1,16 @@
+package timothyyudi.ituneslist;
+
+/**
+ * Created by root on 12/20/2016.
+ */
+
+public class SongModel {
+
+    public String songTitle, songAlbum, imageURL;
+
+    public SongModel(String songTitle, String songAlbum, String imageURL) {
+        this.songTitle = songTitle;
+        this.songAlbum = songAlbum;
+        this.imageURL = imageURL;
+    }
+}
